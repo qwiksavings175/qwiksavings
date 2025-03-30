6d923770d0d1cb8f6fd8b021738137a7e91b9aac
Sparshlohana <sparsh.lohana@gmail.com>
2025-03-30 17:41:10 +0530

    minor bug fix

--- app/api/axios/axios.ts
+++ app/api/axios/axios.ts
@@ -3,14 +3,14 @@ import axios from "axios";
 // Create an axios instance
 
 // For development
-export default axios.create({
-  baseURL: "http://localhost:3000/api",
-});
+// export default axios.create({
+//   baseURL: "http://localhost:3000/api",
+// });
 
 // For production
-// export default axios.create({
-//   baseURL: `https://qwiksavings.com/api`,
-// });
+export default axios.create({
+  baseURL: `https://qwiksavings.com/api`,
+});
 
 // for vercel preview - Staging (always commit this line)
 // export default axios.create({
